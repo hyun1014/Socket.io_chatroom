8c8a23706f6edc3cbfe50d7c9e25104fb63e943d
hyun1014 <ksp70712@gmail.com>
2020-06-29 16:12:00 +0900

    Chat room 개수 추가

--- chat_app.js
+++ chat_app.js
@@ -9,12 +9,9 @@ const io = new sockio(httpServer);
 const http = require('http').Server(app);
 const io = require('socket.io')(http); */ //줄이면 이렇게 됨
 
-const body_parse = require('body-parser');
-
 app.set('view engine', 'ejs');
 
-var rooms = ['room_0', 'room_1'];
-var target = 0;
+var rooms = ['room_0', 'room_1', 'room_2', 'room_3', 'room_4'];
 
 app.get('/', (req, res) => {
     res.render('chat_room.ejs');
@@ -22,24 +19,24 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('user disconnected'); // 누가 나갔는지 알게 할수는 없을까
     });
 
     socket.on('joinRoom', (name, num) => {
-        socket.join(rooms[num], () =>{
+        socket.join(rooms[num], () =>{ // chat room에 join시킨 후, chat room에 joinRoom event emit
             console.log('User ' + name + ' has joined ' + rooms[num]);
             io.to(rooms[num]).emit('joinRoom', name, num);
         });
     });
 
     socket.on('leaveRoom', (name, num) => {
-        socket.leave(rooms[num], () => {
+        socket.leave(rooms[num], () => { // chat room에서 leave시킨 후, chat room에 leaveRoom event emit
             console.log('User ' + name + ' has left ' + rooms[num]);
             io.to(rooms[num]).emit('leaveRoom', name, num);
         });
     });
 
-    socket.on('chat message', (msg, name, num) => {
+    socket.on('chat message', (msg, name, num) => { // msg 받은거 console에 출력 후, chat room에 chat message event emit
         console.log("User: " + name + " / Room: " + rooms[num]);
         console.log("Msg: " + msg);
         io.to(rooms[num]).emit('chat message', msg, name, num);
